f17ebb8a00a03ed00c4466e9c78e527531883105
Alexandr Katunin <Kesha74r@gmail.com>
2022-09-29 15:15:57 +0500

    Добавил первую задачу

---
+++ С#/ДЗ к семинару №2/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+/* 
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число:  ");
+
+string input = Console.ReadLine();
+
+int num = int.Parse(input);
+
+if((num < 1000) & (num > 99))
+{
+
+    num = ((num / 10) % 10);
+
+        Console.WriteLine(num);
+}
+
+else
+   {
+
+Console.WriteLine("Число не трехзначное");
+
+
+   }
+ */
+    
+
+
